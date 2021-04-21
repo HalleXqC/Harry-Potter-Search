e7e6fd976a9c7deebc2dcf0336a10ac38b8fac97
Witcher's Fan <superanvar11@gmail.com>
2021-04-21 17:48:02 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,418 @@
+const database = [
+    {
+        name: 'Harry Potter',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Daniel Radcliffe',
+        image: 'http://hp-api.herokuapp.com/images/harry.jpg'
+    },
+    {
+        name: 'Hermione Granger',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Gryffindor',
+        actor: 'Emma Watson',
+        image: 'http://hp-api.herokuapp.com/images/hermione.jpeg'
+    },
+    {
+        name: 'Ron Weasley',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Rupert Grint',
+        image: 'http://hp-api.herokuapp.com/images/ron.jpg'
+    },
+    {
+        name: 'Draco Malfoy',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Slytherin',
+        actor: 'Tom Felton',
+        image: 'http://hp-api.herokuapp.com/images/draco.jpg'
+    },
+    {
+        name: 'Minerva McGonagall',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Gryffindor',
+        actor: 'Dame Maggie Smith',
+        image: 'http://hp-api.herokuapp.com/images/mcgonagall.jpg'
+    },
+    {
+        name: 'Cedric Diggory',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Hufflepuff',
+        actor: 'Robert Pattinson',
+        image: 'http://hp-api.herokuapp.com/images/cedric.png'
+    },
+    {
+        name: 'Cho Chang',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Ravenclaw',
+        actor: 'Katie Leung',
+        image: 'http://hp-api.herokuapp.com/images/cho.jpg'
+    },
+    {
+        name: 'Severus Snape',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Slytherin',
+        actor: 'Alan Rickman',
+        image: 'http://hp-api.herokuapp.com/images/snape.jpg'
+    },
+    {
+        name: 'Rubeus Hagrid',
+        species: 'half-giant',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Robbie Coltrane',
+        image: 'http://hp-api.herokuapp.com/images/hagrid.png'
+    },
+    {
+        name: 'Neville Longbottom',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Matthew Lewis',
+        image: 'http://hp-api.herokuapp.com/images/neville.jpg'
+    },
+    {
+        name: 'Luna Lovegood',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Ravenclaw',
+        actor: 'Evanna Lynch',
+        image: 'http://hp-api.herokuapp.com/images/luna.jpg'
+    },
+    {
+        name: 'Ginny Weasley',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Gryffindor',
+        actor: 'Bonnie Wright',
+        image: 'http://hp-api.herokuapp.com/images/ginny.jpg'
+    },
+    {
+        name: 'Sirius Black',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Gary Oldman',
+        image: 'http://hp-api.herokuapp.com/images/sirius.JPG'
+    },
+    {
+        name: 'Remus Lupin',
+        species: 'werewolf',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'David Thewlis',
+        image: 'http://hp-api.herokuapp.com/images/lupin.jpg'
+    },
+    {
+        name: 'Arthur Weasley',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Mark Williams',
+        image: 'http://hp-api.herokuapp.com/images/arthur.jpg'
+    },
+    {
+        name: 'Bellatrix Lestrange',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Slythering',
+        actor: 'Helena Bonham Carter',
+        image: 'http://hp-api.herokuapp.com/images/bellatrix.jpg'
+    },
+    {
+        name: 'Lord Voldemort',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Slytherin',
+        actor: 'Ralph Fiennes',
+        image: 'http://hp-api.herokuapp.com/images/voldemort.jpg'
+    },
+    {
+        name: 'Horace Slughorn',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Slytherin',
+        actor: 'Jim Broadbent',
+        image: 'http://hp-api.herokuapp.com/images/slughorn.JPG'
+    },
+    {
+        name: 'Kingsley Snacklebolt',
+        species: 'human',
+        gender: 'male',
+        faculty: '',
+        actor: 'George Harris',
+        image: 'http://hp-api.herokuapp.com/images/kingsley.jpg'
+    },
+    {
+        name: 'Dolores Umbridge',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Slytherin',
+        actor: 'Imelda Staunton',
+        image: 'http://hp-api.herokuapp.com/images/umbridge.jpg'
+    },
+    {
+        name: 'Lucius Malfoy',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Slytherin',
+        actor: 'Jason Isaacs',
+        image: 'http://hp-api.herokuapp.com/images/lucius.jpg'
+    },
+    {
+        name: 'Vincent Crabbe',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Slytherin',
+        actor: 'Jamie Waylett',
+        image: 'http://hp-api.herokuapp.com/images/crabbe.jpg'
+    },
+    {
+        name: 'Gregory Goyle',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Slytherin',
+        actor: 'Josh Herdman',
+        image: 'http://hp-api.herokuapp.com/images/goyle.jpg'
+    },
+    {
+        name: 'Mrs Norris',
+        species: 'cat',
+        gender: 'female',
+        faculty: '',
+        actor: 'Maxime, Atlanis and Tommy the cats',
+        image: 'http://hp-api.herokuapp.com/images/norris.JPG'
+    },
+    {
+        name: 'Argus Filch',
+        species: 'human',
+        gender: 'male',
+        faculty: '',
+        actor: 'David Bradley',
+        image: 'http://hp-api.herokuapp.com/images/filch.jpg'
+    },
+    {
+        name: 'Fred Weasley',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'James Phelps',
+        image: 'https://pbs.twimg.com/profile_images/1334581987030523916/BoGhYhc6.jpg'
+    },
+    {
+        name: 'George Weasley',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Oliver Phelps',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/3/34/George.jpg/revision/latest/top-crop/width/360/height/450?cb=20111113145725&path-prefix=ru'
+    },
+    {
+        name: 'Dobby',
+        species: 'house-elf',
+        gender: 'male',
+        faculty: '',
+        actor: 'Toby Jones',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/2/2c/File_467021.jpg/revision/latest/top-crop/width/360/height/450?cb=20140310113242&path-prefix=ru'
+    },
+    {
+        name: 'Narcissa Malfoy',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Slytherin',
+        actor: 'Helen McCrory',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/c/c3/HnG5xqtMlyk.jpg/revision/latest/top-crop/width/360/height/450?cb=20151112205319&path-prefix=ru'
+    },
+    {
+        name: 'Gellert Grindelwald',
+        species: 'human',
+        gender: 'male',
+        faculty: 'School',
+        actor: 'Johnny Depp',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/7/76/Gellert_Grindelwald_publicity_still.jpg/revision/latest?cb=20190713163752'
+    },
+    {
+        name: 'Pansy Parkinson',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Slytherin',
+        actor: 'Scarlett Byrne',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/8/81/Pansy_Parkinson_infobox.png/revision/latest?cb=20141024191624'
+    },
+    {
+        name: 'Sorting Hat',
+        species: 'hat',
+        gender: 'male',
+        faculty: '',
+        actor: 'Leslie Phillips',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/6/62/Sorting_Hat.png/revision/latest/scale-to-width-down/1000?cb=20161120072849'
+    },
+    {
+        name: 'Hedwig',
+        species: 'owl',
+        gender: 'female',
+        faculty: '',
+        actor: 'Gizmo',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/1/1e/Hedwig_Snowy_Owl_PM.png/revision/latest/scale-to-width-down/936?cb=20161123234010'
+    },
+    {
+        name: 'Peter Pettigrew',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Timothy Spall',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/e/e4/Pettigrew_DH1.jpg/revision/latest/scale-to-width-down/252?cb=20160531030113'
+    },
+    {
+        name: 'Nymphadora Tonks',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Hufflepuff',
+        actor: 'Natalia Tena',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/1/18/Tonks_%281%29.jpg/revision/latest/scale-to-width-down/1000?cb=20170217163322&path-prefix=ru'
+    },
+    {
+        name: 'Albus Dumbledore',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Michael Gambon',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/4/40/Albus_Dumbledore_%28HBP_promo%29_3.jpg/revision/latest/scale-to-width-down/1000?cb=20150822232849'
+    },
+    {
+        name: 'Lily J. Potter',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Gryffindor',
+        actor: 'Geraldine Somerville',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/c/ce/Lily_Potter1.jpg/revision/latest/scale-to-width-down/350?cb=20140712171247'
+    },
+    {
+        name: 'Alastor Moody',
+        species: 'human',
+        gender: 'male',
+        faculty: '',
+        actor: 'Brendan Gleeson',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/4/4d/MoodyProfile.png/revision/latest/scale-to-width-down/473?cb=20200418003242'
+    },
+    {
+        name: 'Dudley Dursley',
+        species: 'human',
+        gender: 'male',
+        faculty: '',
+        actor: 'Harry Melling',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/1/19/Dudley_DH.png/revision/latest/scale-to-width-down/270?cb=20200413050209'
+    },
+    {
+        name: 'Myrtle Warren',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Ravenclaw',
+        actor: 'Shirley Henderson',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/b/b8/Myrtle_Warren_profile.png/revision/latest/scale-to-width-down/1000?cb=20170113020338'
+    },
+    {
+        name: 'Dean Thomas',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Alfred Enoch',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/b/b1/Alfred-enoch-dean-thomas-devon-1688249-2000x2669.jpg/revision/latest?cb=20170617152228&path-prefix=ru'
+    },
+    {
+        name: 'Sybill Trelawney',
+        species: 'human',
+        gender: 'female',
+        faculty: 'Ravenclaw',
+        actor: 'Emma Thompson',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/d/d5/Trelawney.jpg/revision/latest?cb=20090911062559&path-prefix=ru'
+    },
+    {
+        name: 'Oliver Wood',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Sean Biggerstaff',
+        image: 'http://pm1.narvii.com/7054/d8294880d521244b570811cb356f23a211a5f6ecr1-1200-1116v2_uhq.jpg'
+    },
+    {
+        name: 'Seamus Finnigan',
+        species: 'human',
+        gender: 'male',
+        faculty: 'Gryffindor',
+        actor: 'Devon Murray',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/7/7c/DevonMurray07.jpg/revision/latest?cb=20110622051233&path-prefix=ru'
+    },
+
+]
+
+// DOM
+const container = document.querySelector('.data-container');
+let nameInput = document.querySelector('.nameInput');
+let facultyInput = document.querySelector('.facultyInput');
+
+// Window loading
+
+window.addEventListener('load', showCards(database))
+
+
+
+// Функция
+function showCards(arr){
+    const cards = arr.map(({name, species, gender, faculty, actor, image}) => {
+        return `
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4 item card_item">
+                <div class="card-header">
+                    <h5 class="card-title text-center">${name}</h5>
+                </div>
+                <div class="card-body">
+                    <div class="card-body-front">
+                        <div class="card-image" style="background: url('${image}') top / cover no-repeat"></div>
+                        <div class="card_body_list">
+                            <ul>
+                                <li>Species: ${species}</li>
+                                <li>Gender: ${gender}</li>
+                                <li>Faculty: ${faculty}</li>
+                                <li>Actor: ${actor}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-body-back">
+                        <h5>Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info Информация Info</h5>
+                        <a href="https://en.wikipedia.org/wiki/${actor}" target="_blank">About Actor</a>
+                        <button class="addFav"><i class="far fa-heart heartLogo"></i></button>
+                    </div>
+                </div> 
+            </div>
+        `
+    }).join('');
+
+    container.innerHTML = cards
+}
+
+// Search
+
+nameInput.addEventListener('input', e => {
+    const value = e.target.value.toUpperCase();
+    facultyInput = "";
+
+    const filteredArr = database.filter(({name}) => name.toUpperCase().includes(value))
+    showCards(filteredArr)
+})
+
+facultyInput.addEventListener('input', e => {
+    const value = e.target.value.toUpperCase();
+    nameInput = "";
+
+    const filteredArr = database.filter(({faculty}) => faculty.toUpperCase().includes(value))
+    showCards(filteredArr)
+})
+
